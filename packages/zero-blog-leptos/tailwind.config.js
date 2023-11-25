45109eb432f5805a7420adf9693260defba6d577
Jigz <jigzpalillo@gmail.com>
2023-11-25 13:45:10 +0800

    [zero-blog-leptos] Update tailwind.config.js

--- packages/zero-blog-leptos/tailwind.config.js
+++ packages/zero-blog-leptos/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: {
-    files: ['*.html', './src/**/*.rs'],
+    files: ['*.html', '*.css', './src/**/*.rs'],
   },
   theme: {
     extend: {},
